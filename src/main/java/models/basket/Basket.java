a53a001247bdfbef2a950c5d481e17cd0b3127af
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-24 17:15:38 +0100

    adds basket class

--- src/main/java/models/basket/Basket.java
+++ src/main/java/models/basket/Basket.java
@@ -1,4 +1,29 @@
 package models.basket;
 
+import models.stock.Stock;
+
+import java.util.List;
+
 public class Basket {
+    private int id;
+    private List<Stock> stock;
+
+    public Basket() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<Stock> getStock() {
+        return stock;
+    }
+
+    public void setStock(List<Stock> stock) {
+        this.stock = stock;
+    }
 }
