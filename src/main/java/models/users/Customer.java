4f6b8afcf10c5430b9443686fed4c12e27510fc0
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-24 16:56:32 +0100

    Adds customer class

--- src/main/java/models/users/Customer.java
+++ src/main/java/models/users/Customer.java
@@ -1,4 +1,43 @@
 package models.users;
 
+import models.basket.Basket;
+import models.stock.Stock;
+
+import java.util.List;
+
 public class Customer {
+    private List<Stock> purchaseHistory;
+    private String username;
+    private Basket basket;
+
+    public Customer(String username) {
+        this.username = username;
+    }
+
+    public Customer() {
+    }
+
+    public List<Stock> getPurchaseHistory() {
+        return purchaseHistory;
+    }
+
+    public void setPurchaseHistory(List<Stock> purchaseHistory) {
+        this.purchaseHistory = purchaseHistory;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Basket getBasket() {
+        return basket;
+    }
+
+    public void setBasket(Basket basket) {
+        this.basket = basket;
+    }
 }
