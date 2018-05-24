5cb810e71f3b28e0760c442419e827056be8293a
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-24 16:42:32 +0100

    Adds User abstract class

--- src/main/java/models/users/User.java
+++ src/main/java/models/users/User.java
@@ -1,4 +1,29 @@
 package models.users;
 
-public class User {
+public abstract class User {
+    private int id;
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
