bab0139308d87db602c465ac73dba6dbf3c255d2
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-27 18:15:17 +0100

    Equiment index.vtl now lists all equipment stock

--- src/main/java/controllers/EquipmentController.java
+++ src/main/java/controllers/EquipmentController.java
@@ -1,9 +1,14 @@
 package controllers;
 
+import db.DBHelper;
+import models.stock.Stock;
+import models.stock.StockType;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import static spark.Spark.get;
 
@@ -17,6 +22,14 @@ public class EquipmentController {
 
         get("/equipment", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
+            List<Stock> allStock = DBHelper.getAll(Stock.class);
+            List<Stock> equipment = new ArrayList<>();
+            for (Stock item : allStock) {
+                if (item.getType() == StockType.EQUIPMENT) {
+                    equipment.add(item);
+                }
+            }
+            model.put("equipment", equipment);
             model.put("template", "templates/equipment/index.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
