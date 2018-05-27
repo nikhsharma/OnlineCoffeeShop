2ad2d3d4adb6067319fec8c2d92582e287ccd1fc
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-27 20:03:02 +0100

    Adds minor styling to homepage

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -8,12 +8,15 @@ import spark.template.velocity.VelocityTemplateEngine;
 import java.util.HashMap;
 
 import static spark.Spark.get;
+import static spark.SparkBase.staticFileLocation;
 
 public class MainController {
 
     public static void main(String[] args) {
 
         Seeds.seedData();
+        staticFileLocation("/public");
+
 
         MiscController miscController = new MiscController();
         CoffeeController coffeeController = new CoffeeController();
