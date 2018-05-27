de538f0d548d734b51c3da5777bb72d71e547f12
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-27 21:41:12 +0100

    Adds login page (yet to be implemented)

--- src/main/java/controllers/MainController.java
+++ src/main/java/controllers/MainController.java
@@ -2,10 +2,12 @@ package controllers;
 
 
 import db.DBHelper;
+import models.users.User;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 
 import static spark.Spark.get;
 import static spark.SparkBase.staticFileLocation;
@@ -17,13 +19,14 @@ public class MainController {
         Seeds.seedData();
         staticFileLocation("/public");
 
-
+        LoginController loginController = new LoginController();
         MiscController miscController = new MiscController();
         CoffeeController coffeeController = new CoffeeController();
         EquipmentController equipmentController = new EquipmentController();
 
         get("/", (req, res) -> {
             HashMap<String, Object> model = new HashMap<>();
+            List<User> users = DBHelper.getAll(User.class);
             model.put("template", "templates/main.vtl");
             return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
