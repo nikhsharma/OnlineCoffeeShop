708ed54ab8586fc173157377d8addbdce44ab33a
Daniel Childs <dc.1994@outlook.com>
2018-05-28 15:13:50 +0100

    can save a new item to the website using post stock/management

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -6,10 +6,13 @@ import models.stock.StockType;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 import static spark.Spark.get;
+import static spark.Spark.post;
 
 public class AdminController {
 
@@ -30,12 +33,32 @@ public class AdminController {
 //    new
         get("stock-management/new", (req, res) -> {
         HashMap<String, Object> model = new HashMap<>();
-        List<StockType> stockType = DBHelper.getAll(StockType.class);
-        model.put("stockType", stockType);
+        StockType arr[] = StockType.values();
+        model.put("stockType", arr);
         model.put("template", "templates/user/create.vtl");
         return new ModelAndView(model, "templates/layout.vtl");
         }, new VelocityTemplateEngine());
 
+// create
+        post("/stock-management",(req, res) -> {
+            String description = req.queryParams("description");
+            String typeDescription = req.queryParams("type");
+            StockType stockType = StockType.valueOf(typeDescription);
+            String type = req.queryParams("type");
+            double price = Double.parseDouble(req.queryParams("price"));
+            int quantity = Integer.parseInt(req.queryParams("quantity"));
+            Stock stock = new Stock(description, stockType, price, quantity);
+            DBHelper.save(stock);
+            res.redirect("/stock-management");
+            return null;
+        }, new VelocityTemplateEngine());
+
+//
+//        post("stock-management/:id/delete", (req, res) -> {
+//            int id = Integer.parseInt(req.queryParams("id"));
+//
+//
+//        }, new VelocityTemplateEngine());
 //        create functions above this line
     }
 
