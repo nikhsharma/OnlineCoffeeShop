a1c3c34f0f14fed083678c623ef5bbb6ec53912d
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-28 10:15:58 +0100

    Fixes issue where removeItem() wasn't updating db

--- src/main/java/models/basket/Basket.java
+++ src/main/java/models/basket/Basket.java
@@ -64,6 +64,7 @@ public class Basket {
             if (originalStock.getDescription() == item.getDescription()) {
                 quantity =  item.getQuantity();
                 item.setBasket(null);
+                DBHelper.save(item);
                 stock.remove(item);
             }
         }
